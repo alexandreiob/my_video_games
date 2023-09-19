b5673b6b08f4cac3d23860d4f69cacdac076cb65
alexandreiob <aeight.design@gmail.com>
2023-09-19 12:44:10 +0200

    premiercommit

---
+++ src/Controller/AdminerController.php
@@ -0,0 +1,20 @@
+<?php
+
+// src/Controller/AdminerController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminerController extends AbstractController
+{
+    /**
+     * @Route("/adminer", name="adminer")
+     */
+    public function index(): Response
+    {
+        return $this->render('adminer/index.html.twig');
+    }
+}
+
